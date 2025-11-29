a31ca2576c554106a5d4dcff1f0b4f32feaf7c46
kiran shahi <shahikiran575@gmail.com>
2025-11-29 22:41:31 +0545

    update customreact

--- custom-react/customreact.js
+++ custom-react/customreact.js
@@ -1,29 +1,30 @@
-function customRender(reactElement, Container) {
-  // const domElement = document.createElement(reactElement.type);
-  // domElement.innerHTML = reactElement.children;
-  // domElement.setAttribute("href", reactElement.props.href);
-  // domElement.setAttribute("target", reactElement.props.target);
-  // mainContainer.appendChild(domElement);
+function customRender(reactElement,maincontainer){
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href',reactElement.props.href)
+    domElement.setAttribute('target',reactElement.props.target)
+    maincontainer.appendChild(domElement)
 
-  const domElement = document.createElement(reactElement.type);
-  domElement.innerHTML = reactElement.children;
-  for (const prop in reactElement.props) {
-    if (prop === "children") continue;
-
-    domElement.setAttribute(prop, reactElement.props[prop]);
-  }
-  Container.appendChild(domElement);
 }
+
+// const domElement = document.createElement(reactElement.type)
+// domeElement.innerHTML = reactElement.children
+
+// for (const prop in reactElement.props){
+//     if(prop==='children') continue;
+//     domeElement.setAttribute(prop,reactElement.props[prop])
+// }
+
 
 const reactElement = {
   type: "a",
   props: {
     href: "https://google.com",
-    target: "_blank",
+    target: "blank",
   },
-  children: "Click me to visit google",
+  children: "Click me to visit google ",
 };
 
-const mainContainer = document.querySelector("#root");
+const mainContainer = document.querySelector(".root");
 
-customRender(reactElement, mainContainer);
+customRender(reactElement,mainContainer)// what to inject reactElement where in maincontainer
